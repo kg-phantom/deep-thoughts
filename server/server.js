f6574e1fcefcd74f235ea47bdaa3ed3bfe7600b7
kg-phantom <ksurbayan@gmail.com>
2022-01-05 23:55:03 -0800

    thoughts and friend mutations

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -11,7 +12,8 @@ const startServer = async () => {
   // create a new Apollo server and pass in schema data
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware
   });
   
   // start Apollo server
