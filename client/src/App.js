3a8cfda25a63ad7953e00636af3a63548e08db47
kg-phantom <ksurbayan@gmail.com>
2022-01-07 18:14:41 -0800

    login, signup, and view profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -11,13 +12,23 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
 import Home from './pages/Home';
+import auth from './utils/auth';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
